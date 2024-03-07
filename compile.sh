da1514ef42ffd25db412898184ba643eba73449b
Alessandro Catenacci <alessandrocatenacci22@gmail.com>
2024-03-07 18:58:23 +0100

    Improved project structure.

--- compile.sh
+++ compile.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Clean up previous builds (optional but recommended for a clean build)
-rm -rf out/*
+# Directory setup
+outputDir=out
 
-# Create the output directory if it doesn't exist
-mkdir -p out
+# Clean up previous builds
+latexmk -c -outdir=$outputDir
+rm -rf "${outputDir:?}"/*
 
-# Compile the LaTeX document, this involves multiple steps
+# Ensure the output directory exists
+mkdir -p $outputDir
 
-# Step 1: Compile the main document. This generates aux files needed for bibliography
-pdflatex -output-directory=out -interaction=nonstopmode tesi.tex
+# Compile the document
+latexmk -f -pdf -outdir=$outputDir -interaction=nonstopmode tesi.tex
 
-# Step 2: Run bibtex/biber to process bibliography. Adjust if using biber
-biber out/tesi
+echo "Compilation completed. Check the '$outputDir' directory for the output."
 
-# Step 3: Run pdflatex again to incorporate bibliography references and resolve cross-references
-pdflatex -output-directory=out -interaction=nonstopmode tesi.tex
-pdflatex -output-directory=out -interaction=nonstopmode tesi.tex
-
-# Note: The script runs pdflatex multiple times because LaTeX needs multiple passes to
-# resolve all references correctly, especially when dealing with bibliographies and cross-references.
-
-echo "Compilation completed. Check the 'out' directory for the output."
